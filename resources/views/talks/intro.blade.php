a4c41641778ca6a4669be3404bbf4a739e43686b
Paulo Eduardo <pauloelr@gmail.com>
2016-01-29 17:39:26 -0200

    Check Websocket Address

--- resources/views/talks/intro.blade.php
+++ resources/views/talks/intro.blade.php
@@ -2,6 +2,10 @@
 	<h2>5º Seminário Locaweb PHPSP!</h2>
 	<h3>Smartphone / Tablet / Notebook</h3>
 	<p><img src="images/navegadores.png" /></p>
-	<p><a href="http://seminariophpsp.jelasticlw.com.br/">http://seminariophpsp.jelasticlw.com.br/</a></p>
+	<?php if (strpos($host, 'jelasticlw') === false): ?>
+		<p><a href="http://seminario.phpsp.org.br/">http://seminario.phpsp.org.br</a></p>
+	<?php else: ?>
+		<p><a href="http://seminariophpsp.jelasticlw.com.br//">http://seminariophpsp.jelasticlw.com.br</a></p>
+	<?php endif; ?>
 	<h4><a href="https://github.com/PHPSP/docs/blob/master/codigo-de-conduta.md">Código de Conduta</a></h4>
 </section>
