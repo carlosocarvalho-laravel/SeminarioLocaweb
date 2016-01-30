e1d3dc85e712054a6bef11df657289432e2579a8
Marcel dos Santos <marcelgsantos@gmail.com>
2016-01-30 00:06:40 -0200

    Altera slide de introdução adicionando logos, links e informações sobre wi-fi

--- resources/views/talks/intro.blade.php
+++ resources/views/talks/intro.blade.php
@@ -1,7 +1,24 @@
 <section>
-	<h2>5º Seminário Locaweb PHPSP!</h2>
-	<h3>Smartphone / Tablet / Notebook</h3>
-	<p><img src="images/navegadores.png" /></p>
-	<p><a href="http://seminariophpsp.jelasticlw.com.br/">http://seminariophpsp.jelasticlw.com.br/</a></p>
-	<h4><a href="https://github.com/PHPSP/docs/blob/master/codigo-de-conduta.md">Código de Conduta</a></h4>
+	
+	<div class="logos">
+		<img class="logo-locaweb" src="images/logo-locaweb.png">
+		<img class="logo-phpsp" src="images/logo-phpsp.png">
+		<div style="clear: both"></div>
+	</div>
+
+	<h2>5º Seminário Locaweb PHPSP</h2>
+
+	<p>
+		<a class="custom-link" style="text-transform: lowercase" href="http://seminariophpsp.jelasticlw.com.br/">http://seminariophpsp.jelasticlw.com.br/</a>
+	</p>
+
+	<p class="custom-links">
+		<a class="custom-link" href="http://bit.ly/avalie-phpsp">Joind.in</a>
+		<a class="custom-link" href="https://github.com/PHPSP/docs/blob/master/codigo-de-conduta.md">Código de Conduta</a>
+		<a class="custom-link" href="http://bit.ly/slack-phpsp">Slack PHPSP</a>
+	</p>
+
+	<p class="wifi">
+		<strong>Wi-fi:</strong> Locaweb/phpsp2016
+	</p>
 </section>
